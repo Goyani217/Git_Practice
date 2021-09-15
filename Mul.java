dd262f208c0aff5d0cf3488f34073790a421cb6e
Goyani217 <jayk.goyani@gmail.com>
2021-09-15 16:41:22 +0530

    Mul.java file is fully functional

---
+++ Mul.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number1: ");
+		int a = sc.nextInt();
+		System.out.println("Enter a number2: ");
+		int b = sc.nextInt();
+		System.out.println("Mul is : "+ (a*b));
+	}
+}
