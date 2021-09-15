2dbaa294acaf96ba1da3a20823310b5239064f60
Goyani217 <jayk.goyani@gmail.com>
2021-09-15 12:06:52 +0530

    Added a new file Diff.java and modified Add.java file

--- Add.java
+++ Add.java
@@ -5,7 +5,9 @@ public class Add {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number1: ");
 		int a = sc.nextInt();
+		System.out.println("Enter a number2: ");
 		int b = sc.nextInt();
 		System.out.println("Sum is : "+ (a+b));
 	}
